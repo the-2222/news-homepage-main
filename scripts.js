76f51a93ae371b911d2cf51ccbec7a3e8e155e9e
the-2222 <franekjasinski543@gmail.com>
2026-05-22 11:03:05 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const mobileMenu = document.getElementById("Kebab");
+
+function closeMenu() {
+	mobileMenu.style.visibility = 'collapse';
+}
+
+function openMenu() {
+	console.log("abc");
+	mobileMenu.style.visibility = 'visible';
+}
